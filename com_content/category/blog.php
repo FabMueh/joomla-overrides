fa529d98433506901cade6369d7c0e9ec9c5e470
Fabian Mühlpfordt <fabian.muehlpfordt@eonerc.rwth-aachen.de>
2021-03-06 12:27:27 +0100

    add pagination to blog.php

--- com_content/category/blog.php
+++ com_content/category/blog.php
@@ -15,8 +15,16 @@ JHtml::_('behavior.caption');
 <div class="blog-wrapper">
     <?php foreach($this->item as $post) : ?>
         <div class="blog-post">
-            
+
         </div>
     <?php endforeach; ?>
+
+    <?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
+		<div class="pagination">
+			<?php if ($this->params->def('show_pagination_results', 1)) : ?>
+				<p class="counter pull-right"> <?php echo $this->pagination->getPagesCounter(); ?> </p>
+			<?php endif; ?>
+			<?php echo $this->pagination->getPagesLinks(); ?> </div>
+	<?php endif; ?>
 </div>
 
