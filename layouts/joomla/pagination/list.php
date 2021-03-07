2ced8eb5185ef2fdff8f83ee8bd50f2240e7ab60
Fabian Mühlpfordt <fabian.muehlpfordt@eonerc.rwth-aachen.de>
2021-03-07 12:29:51 +0100

    update pagination override

---
+++ layouts/joomla/pagination/list.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$list = $displayData['list'];
+?>
+<ul>
+	<li class="pagination-start"><?php echo $list['start']['data']; ?></li>
+	<?php foreach ($list['pages'] as $page) : ?>
+		<?php echo '<li>' . $page['data'] . '</li>'; ?>
+	<?php endforeach; ?>
+	<li class="pagination-end"><?php echo $list['end']['data']; ?></li>
+</ul>
